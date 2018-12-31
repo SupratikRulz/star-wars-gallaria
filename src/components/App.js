3a055793d967933b6d759efdba4db6918e00ed60
Supratik <supratik@fusioncharts.com>
2018-12-31 12:16:32 +0530

    Initial setup for redux

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import Navbar from './Navbar';
+import Home from './Home';
+import People from './People';
+import Films from './Films';
+
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/home' component={Home}/>
+            <Route path='/people' component={People}/>
+            <Route path='/films' component={Films}/>
+            <Redirect from='/' to='/home'/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
