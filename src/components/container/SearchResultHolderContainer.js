cab7d47c8b3e60562f1200aab14dae862e944e1c
Supratik <supratik@fusioncharts.com>
2019-01-01 22:56:06 +0530

    Removed updateSearch method to be passed as props

--- src/components/container/SearchResultHolderContainer.js
+++ src/components/container/SearchResultHolderContainer.js
@@ -1,15 +1,9 @@
 import { connect } from 'react-redux';
 import SearchResultHolder from './../SearchResultHolder';
-import { setSearchKey } from './../../actions/people.action';
 
 const mapStateToProps = state => ({
   characters: state.people.characters,
   searchKey: state.people.searchKey
 });
 
-const mapDispatchToProps = dispatch => ({
-  updateSearch: (searchKey) => dispatch(setSearchKey(searchKey))
-});
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchResultHolder);
+export default connect(mapStateToProps)(SearchResultHolder);
