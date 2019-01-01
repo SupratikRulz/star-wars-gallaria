be1c726d32f7651fc753d91bcc6c88016e6b8acd
Supratik <supratik@fusioncharts.com>
2019-01-01 11:21:01 +0530

    Added some constants for initial state and actions type

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const initialState = {
+  films: [],
+  characters: [],
+  searchKey: '',
+  searchCardStates: {}
+};
+
+export const ADD_FILMS = 'ADD_FILMS';
+export const ADD_PEOPLE = 'ADD_PEOPLE';
