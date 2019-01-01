75ba9b9e255d828be1b80cf26604876f5d8ab31e
Supratik <supratik@fusioncharts.com>
2019-01-02 00:33:21 +0530

    Added proptypes and default props to card component

--- src/components/atomic/Card.js
+++ src/components/atomic/Card.js
@@ -1,18 +1,38 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import './../css/Card.css';
 
 const Card = ({
-    title = '',
-    content = '',
-    cardClasses = '',
-    cardTitleClasses = '',
-    cardContentClasses = ''
+    title,
+    content,
+    cardClasses,
+    cardTitleClasses,
+    cardContentClasses
   }) => (
-    <div className={`card ${cardClasses}`}>
-      <div className={`card-title ${cardTitleClasses}`}>{title}</div>
-      <div className={`card-content ${cardContentClasses}`}>{content}</div>
-    </div>
-  );
+  <div className={`card ${cardClasses}`}>
+    <div className={`card-title ${cardTitleClasses}`}>{title}</div>
+    <div className={`card-content ${cardContentClasses}`}>{content}</div>
+  </div>
+);
+
+Card.defaultProps = {
+  title: '',
+  content: '',
+  cardClasses: '',
+  cardTitleClasses: '',
+  cardContentClasses: ''
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element,
+    PropTypes.node
+  ]),
+  cardClasses: PropTypes.string,
+  cardTitleClasses: PropTypes.string,
+  cardContentClasses: PropTypes.string
+}
 
 export default Card;
