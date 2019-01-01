336e8314d170aab34a4530a0fa495fdd354517f3
Supratik <supratik@fusioncharts.com>
2019-01-02 01:12:37 +0530

    Added comments for constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+// Reducer initial state
 export const filmInitialState = {
   films: []
 }
@@ -7,6 +8,7 @@ export const peopleInitialState = {
   searchKey: ''
 }
 
+// Action Types
 export const ADD_FILMS = 'ADD_FILMS';
 export const ADD_PEOPLE = 'ADD_PEOPLE';
 export const UPDATE_SEARCH_KEY = 'UPDATE_SEARCH_KEY'
