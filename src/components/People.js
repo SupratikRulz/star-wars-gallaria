8ee84fce5705cee0ed6df68a53abf3f1f2172be6
Supratik <supratik@fusioncharts.com>
2019-01-01 16:39:45 +0530

    Removed data fetching in people cdm and also removed character

--- src/components/People.js
+++ src/components/People.js
@@ -1,43 +1,13 @@
 import React, { Component } from 'react';
 
 import SearchBox from './atomic/SearchBox';
-import SearchResultContainer from './SearchResultContainer';
+import SearchResultHolder from './container/SearchResultHolderContainer';
 
-
-import { service } from './../services/api';
 
 import './css/People.css';
 import logo from './../starwars-logo.png';
 
 export default class People extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      fetchURI: 'https://swapi.co/api/people/?format=json',
-      peopleResults: [],
-      searchKey: ''
-    }
-  }
-
-  componentDidMount() {
-    service
-      .get(this.state.fetchURI)
-      .then(async _data => {
-        let infoObj = _data;
-        if (infoObj) {
-          let characters = [...infoObj.results];
-          while (infoObj.next) {
-            infoObj = await service.get(infoObj.next);
-            infoObj && (characters = [...characters, ...infoObj.results]);
-            this.setState({
-              peopleResults: characters
-            });
-          }
-        }
-      });
-  }
-
   render() {
     return (
       <div className='container-fluid'>
@@ -45,20 +15,16 @@ export default class People extends Component {
           <div className='col-4 search-box-height'>
             <img src={logo} alt='Star Wars' className='search-logo'/>
             <SearchBox
-              updateSearchKey={this.updateSearchKey}
+              updateSearchKey={this.props.updateSearch}
               searchTitle='Hey fellas! Search your favorite heros here.'
               placeholder='Search here...'
             />
           </div>
           <div className='col-8'>
-            <SearchResultContainer characters={this.state.peopleResults} searchKey={this.state.searchKey}/>
+            <SearchResultHolder/>
           </div>
         </div>
       </div>
     )
-  }
-
-  updateSearchKey = searchKey => {
-    this.setState({searchKey})
   }
 }
