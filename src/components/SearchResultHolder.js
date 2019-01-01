b5f705bdddd441aa1b5dfedf6da631c259930136
Supratik <supratik@fusioncharts.com>
2019-01-01 23:41:24 +0530

    Added proptypes and comments for SearchResultHolder component

--- src/components/SearchResultHolder.js
+++ src/components/SearchResultHolder.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import SearchResultCard from './SearchResultCard';
 import Loader from './atomic/Loader';
-
 import './css/SearchResultContainer.css';
 
 export default class SearchResultHolder extends Component {
@@ -12,10 +11,16 @@ export default class SearchResultHolder extends Component {
     return (
       <>
       <div className='mt-50'>
-        {searchKey ? <div>Searching for characters with "{searchKey}" in name</div> : <div>Showing All Characters</div>}
+        {
+          // Show different texts for empty and valued search keys.
+          searchKey ?
+            <div>Searching for characters with "{searchKey}" in name</div> :
+            <div>Showing All Characters</div>}
       </div>
       <div className='row search-result-container'>
         {
+          // Show Loader component when characters are not fetched
+          // else show the cards for each character.
           loadingCharacters ?
             (
               <>
@@ -25,12 +30,18 @@ export default class SearchResultHolder extends Component {
             ) :
             characters
               .map(_character => {
-                _character.show = searchKey !== '' ? _character.name.toLowerCase().includes(searchKey) : true;
-                return <SearchResultCard key={_character.name+_character.created+_character.mass} {..._character}/>
+                // Show only the results whose name contains the search key
+                let show = searchKey !== '' ? _character.name.toLowerCase().includes(searchKey) : true;
+                return <SearchResultCard key={_character.name+_character.created+_character.mass} {..._character} show={show}/>
               })
         }
       </div>
       </>
     )
   }
+}
+
+SearchResultHolder.propTypes = {
+  characters: PropTypes.array.isRequired,
+  searchKey: PropTypes.string.isRequired
 }
