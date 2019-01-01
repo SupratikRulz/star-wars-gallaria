a0b23dc63e7ac84b4c5311e231e63812b513c0dc
Supratik <supratik@fusioncharts.com>
2019-01-01 11:21:47 +0530

    Created the store with middleware as thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './../reducers';
 
-const store = createStore();
+const store = createStore(
+    rootReducer,
+    {},
+    applyMiddleware(thunk)
+  );
+
 export {store};
