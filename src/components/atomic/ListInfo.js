4a45cc290925f51b6c363c4e01ba46e39410ecee
Supratik <supratik@fusioncharts.com>
2019-01-02 00:31:50 +0530

    Added additional prop seperator which will be used to identify the key. Also added defaultProps and propTypes to ListInfo

--- src/components/atomic/ListInfo.js
+++ src/components/atomic/ListInfo.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './../css/ListInfo.css'
 
-const ListInfo = ({headerName = '', displayFormat = '', keysArr = [], jsonArr = [], headerClassName = '', listContainerClassName = '', listClassName = ''}) => {
+const ListInfo = ({headerName, displayFormat, keysArr, jsonArr, separator, headerClassName, listContainerClassName, listClassName}) => {
   const dataPresent = jsonArr.length > 0;
+  // Display the list only if data is present
   return dataPresent && (
     <>
       <div className={`info-header ${headerClassName}`}>
@@ -12,9 +14,11 @@ const ListInfo = ({headerName = '', displayFormat = '', keysArr = [], jsonArr = 
         {
           jsonArr.map((obj, index) => {
             let displayString = displayFormat;
+            // Extract the keys and put the value of he key in the format specified.
             for(let key of keysArr) {
-              displayString = displayString.replace('$_' + key, obj[key])
+              displayString = displayString.replace(separator + key, obj[key])
             }
+            // Display the text in the format specified.
             return (
               <div key={index+displayString} className={listClassName}>
                 {displayString}
@@ -25,6 +29,28 @@ const ListInfo = ({headerName = '', displayFormat = '', keysArr = [], jsonArr = 
       </div>
     </>
   )
+}
+
+ListInfo.defaultProps = {
+  headerName: '',
+  displayFormat: '',
+  keysArr: [],
+  jsonArr: [],
+  separator: '$_',
+  headerClassName: '',
+  listContainerClassName: '',
+  listClassName: ''
+}
+
+ListInfo.propTypes = {
+  headerName: PropTypes.string,
+  displayFormat: PropTypes.string,
+  keysArr: PropTypes.array,
+  jsonArr: PropTypes.array,
+  separator: PropTypes.string,
+  headerClassName: PropTypes.string,
+  listContainerClassName: PropTypes.string,
+  listClassName: PropTypes.string
 }
 
 export default ListInfo;
