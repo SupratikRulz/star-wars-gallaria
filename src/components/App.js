6b8f4b72dc8c1b10bedd929d0982e87f51c158ea
Supratik <supratik@fusioncharts.com>
2019-01-01 22:18:36 +0530

    Added proptypes and comments for App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
 import Navbar from './Navbar';
 import Home from './Home';
 import People from './container/PeopleContainer';
@@ -10,6 +10,10 @@ import './css/App.css';
 
 class App extends Component {
   componentDidMount() {
+  /**
+   * Fetch the film and people data from server and
+   * write to redux store when the app component is mounted.
+   */
     this.props.fetchFilms();
     this.props.fetchCharacters();
   }
@@ -19,17 +23,32 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+          {
+            /* Routes definitions:
+            *  /       > will direct to Home component
+            *  /home   > will direct to Home component
+            *  /people > will direct to People component
+            *  /films  > will direct to Film component
+            *  /[xyz]  > any other routes will be automatically redirected to Home component
+            */
+          }
           <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/home' component={Home}/>
-            <Route path='/people' component={People}/>
-            <Route path='/films' render={() => <Films films={this.props.films}/>}/>
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home}/>
+            <Route path={`${process.env.PUBLIC_URL}/home`} component={Home}/>
+            <Route path={`${process.env.PUBLIC_URL}/people`} component={People}/>
+            <Route path={`${process.env.PUBLIC_URL}/films`} render={() => <Films films={this.props.films}/>}/>
             <Redirect from='/' to='/home'/>
           </Switch>
         </div>
       </BrowserRouter>
     );
   }
+}
+
+App.propTypes = {
+  films: PropTypes.array.isRequired,
+  fetchFilms: PropTypes.func.isRequired,
+  fetchCharacters: PropTypes.func.isRequired
 }
 
 export default App;
