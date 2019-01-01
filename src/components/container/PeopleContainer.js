95565e8d15ff94edd37340c285e6bc0a0904b372
Supratik <supratik@fusioncharts.com>
2019-01-01 16:38:50 +0530

    Removed data fetcing in cdm

--- src/components/container/PeopleContainer.js
+++ src/components/container/PeopleContainer.js
@@ -3,7 +3,6 @@ import People from './../People';
 import { setSearchKey } from './../../actions/people.action';
 
 const mapStateToProps = state => ({
-  characters: state.peopleReducer.characters,
   searchKey: state.peopleReducer.searchKey
 });
 
